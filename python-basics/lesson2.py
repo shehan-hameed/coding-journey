0ec7ce520c4f66214e4fe3563ea9a14ca1722149
shehan-hameed <shehan2005savain@gmail.com>
2026-02-03 19:00:45 +0530

    Lesson 2: variables and data types

---
+++ python-basics/lesson2.py
@@ -0,0 +1,22 @@
+name = "Shehan"
+age = 20
+print(name)
+
+city = "Colombo"
+marks = 85
+average = 87.5
+
+print(type(name))
+print(type(age))
+print(type(average))
+
+age = 21
+print(age)
+
+
+name = "Shehan"
+age = 20
+degree = "IT&M"
+uni = "University of Moratuwa"
+
+print ("My name is", name, "and I am ", age, " years old. I am studying ", degree, " at ", uni)
