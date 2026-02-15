9fb4ee79f27ba11f3eb7dd251080b48021c8e07f
shehan-hameed <shehan2005savain@gmail.com>
2026-02-15 15:07:22 +0530

    Mini task

--- python-basics/lesson3.py
+++ python-basics/lesson3.py
@@ -1,2 +1,35 @@
+#name = input("Enter your name:")
+#print("Hello," + name)
+
+
+#age = input("Enter your age:")
+#print(type(age))
+
+
+#age = int(input("Enter your age:"))
+#print(age + 5)
+
+
+## I am making the upper code comments to run the lower code line more easily
+
+
+"""7️⃣ Mini Task (Do this yourself) 🎯
+Write a program that:
+Asks for:
+Name
+Age
+Degree
+Prints:
+Hi Shehan, you are 20 years old and studying IT.
+"""
+
+#That up there is a Docstring(multi line comment)
+
+#Mini Task
+
 name = input("Enter your name:")
-print("Hello," + name)
+age = int(input("Enter your age:"))
+degree = input("Enter your degree:")
+
+#print("Hi " + name + ", you are " + str(age) + " years old and studying " + degree + ".")
+print(f"Hi {name} you are {age} years old and studying {degree}.")
